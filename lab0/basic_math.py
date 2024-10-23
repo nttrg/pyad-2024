96b0f580a0481351be8bd6bf74c5aecbaa150f5d
nttrg <113007757+nttrg@users.noreply.github.com>
2024-10-23 19:21:23 +0300

    Update basic_math.py

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy as sc
+from scipy.optimize import minimize_scalar
+from scipy.stats import kurtosis, skew
 
 
 def matrix_multiplication(matrix_a, matrix_b):
@@ -31,7 +33,40 @@ def functions(a_1, a_2):
     Необходимо найти точки экстремума функции и определить, есть ли у функций общие решения.
     Вернуть нужно координаты найденных решения списком, если они есть. None, если их бесконечно много.
     """
-    # put your code here
+    a1 = list(map(float, a_1.split()))
+    a2 = list(map(float, a_2.split()))
+
+    # проверка на бесконечно много решений
+    if a1 == a2:
+        return None
+
+    def F(x):
+        return a1[0] * x**2 + a1[1] * x + a1[2]
+    def P(x):
+        return a2[0] * x**2 + a2[1] * x + a2[2]
+
+    # точки экстремума
+    res_F = minimize_scalar(F)
+    res_P = minimize_scalar(P)
+    extremum_F = res_F.x
+    extremum_P = res_P.x
+
+    extremum_points = []
+    if not np.isnan(extremum_F):
+        extremum_points.append((int(extremum_F), int(F(extremum_F))))
+    if not np.isnan(extremum_P):
+        extremum_points.append((int(extremum_P), int(P(extremum_P))))
+    print( "точки экстремума функции: ", extremum_points)
+
+    roots = np.roots([a1[0] - a2[0], a1[1] - a2[1], a1[2] - a2[2]])
+    real_roots = [r for r in roots if np.isreal(r)]
+
+    solutions = []
+    for r in real_roots:
+        solutions.append((int(r.real), int(np.polyval(a1, r.real))))
+
+    unique_solutions = sorted(set(solutions))
+    return unique_solutions
     pass
 
 
@@ -40,7 +75,12 @@ def skew(x):
     Задание 3. Функция для расчета коэффициента асимметрии.
     Необходимо вернуть значение коэффициента асимметрии, округленное до 2 знаков после запятой.
     """
-    # put your code here
+    n = len(x)
+    mean = np.mean(x)
+    std = np.std(x)
+    m3 = np.sum((x - mean)**3) / n
+    skewness = m3 / std**3
+    return round(skewness, 2)
     pass
 
 
@@ -49,5 +89,10 @@ def kurtosis(x):
     Задание 3. Функция для расчета коэффициента эксцесса.
     Необходимо вернуть значение коэффициента эксцесса, округленное до 2 знаков после запятой.
     """
-    # put your code here
+    n = len(x)
+    mean = np.mean(x)
+    std = np.std(x)
+    m4 = np.sum((x - mean)**4) / n
+    excess_kurtosis = m4 / std**4 - 3
+    return round(excess_kurtosis, 2)
     pass
